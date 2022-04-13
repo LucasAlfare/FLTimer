aeffa6168789194622305e634813b83e405416d4
LucasAlfare <god-lucas@hotmail.com>
2022-04-13 11:57:12 -0300

    some versioning changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+allprojects {
+    /*
+    * Correct format: 'MAJOR.MINOR.BUILD', where:
+    * MAJOR is a non-negative integer with a maximum value of 255;
+    * MINOR is a non-negative integer with a maximum value of 255;
+    * BUILD is a non-negative integer with a maximum value of 65535;
+      * You can specify the correct version using DSL properties: nativeDistributions.windows.msiPackageVersion, nativeDistributions.windows.packageVersion, nativeDistributions.packageVersion
+     */
+}
