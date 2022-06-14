f7f31c2222f4ecdf8c652b8f26158680ad71ac04
LucasAlfare <god-lucas@hotmail.com>
2022-06-13 23:22:19 -0300

    upgrade AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "1.6.10"
 
-    id("com.android.application") version "7.2.0" apply false
-    id("com.android.library") version "7.2.0" apply false
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.5.31" apply false
 }
 
