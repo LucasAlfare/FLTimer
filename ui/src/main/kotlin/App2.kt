9b0caf549d39003a55fb4a50d6db426f61675fbc
LucasAlfare <god-lucas@hotmail.com>
2023-05-04 19:43:51 -0300

    clean up

--- ui/src/main/kotlin/App2.kt
+++ ui/src/main/kotlin/App2.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.lucasalfare.fltimer.core.model.FLTimerState
+import com.lucasalfare.fltimer.core.model.Solve
 import com.lucasalfare.fltimer.ui.FLTimerUiState.Companion.currentTabName
 import com.lucasalfare.fltimer.ui.TabName
 import com.lucasalfare.fltimer.ui.Tabs
@@ -15,21 +17,10 @@ fun App2() {
   Column(modifier = Modifier.fillMaxSize()) {
     Box(modifier = Modifier.weight(1f)) {
       when (currentTabName.value) {
-        TabName.Timer.name -> {
-          Timer()
-        }
-
-        TabName.Solves.name -> {
-          Solves()
-        }
-
-        TabName.Stats.name -> {
-          Text(currentTabName.value)
-        }
-
-        else -> {
-          Text(currentTabName.value)
-        }
+        TabName.Timer.name -> Timer()
+        TabName.Solves.name -> Solves()
+        TabName.Stats.name -> Text(currentTabName.value)
+        else -> Text(currentTabName.value)
       }
     }
 
