f3cfc8976d3e9e195872bea81dee0e1082cfa647
LucasAlfare <god-lucas@hotmail.com>
2023-05-11 22:36:58 -0300

    clean up

--- ui/src/main/kotlin/com/lucasalfare/fltimer/ui/theme/FLTimerThemeLoader.kt
+++ ui/src/main/kotlin/com/lucasalfare/fltimer/ui/theme/FLTimerThemeLoader.kt
@@ -2,11 +2,6 @@ package com.lucasalfare.fltimer.ui.theme
 
 import androidx.compose.ui.text.font.FontFamily
 
-// TODO:
-//  this file should be used to offer abstract functions to
-//  be used to load theme on each platform (desktop, mobile),
-//  mostly due to font loading being different on each one.
-
 class MyFontFamilies {
   companion object {
     lateinit var regular: FontFamily
