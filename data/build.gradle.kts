30e20586e46bb77c713941ab6bcbe2d44819bef0
LucasAlfare <god-lucas@hotmail.com>
2023-12-16 13:53:58 -0300

    adding sqlite and exposed dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -4,3 +4,8 @@ version = "1.0-SNAPSHOT"
 plugins {
   kotlin("jvm")
 }
+
+dependencies {
+  implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+  implementation("org.jetbrains.exposed:exposed-core:0.45.0")
+}
