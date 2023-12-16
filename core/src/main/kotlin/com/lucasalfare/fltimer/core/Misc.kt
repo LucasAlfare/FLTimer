fc3a2c2b65a347dab5cc59e019892bd562ac5921
LucasAlfare <god-lucas@hotmail.com>
2023-12-16 02:18:09 -0300

    remove unused functions

--- core/src/main/kotlin/com/lucasalfare/fltimer/core/Misc.kt
+++ core/src/main/kotlin/com/lucasalfare/fltimer/core/Misc.kt
@@ -9,16 +9,3 @@ import java.util.*
 fun getCurrentTime(useNanos: Boolean = false) =
   if (useNanos) System.nanoTime()
   else System.currentTimeMillis()
-
-fun Date.toString(
-  format: String = "dd/mm/yyyy, HH:mm:ss",
-  locale: Locale = Locale.getDefault()
-): String {
-  val formatter = SimpleDateFormat(format, locale)
-  return formatter.format(this)
-}
-
-fun getCurrentDateTime(): Date {
-  System.currentTimeMillis()
-  return Calendar.getInstance().time
-}
