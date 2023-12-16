10b20b1518c91e58e0e3c82241ca38d4369ba273
LucasAlfare <god-lucas@hotmail.com>
2023-12-16 02:26:18 -0300

    fix import due to new FLListening library API

--- core/src/main/kotlin/com/lucasalfare/fltimer/core/configuration/ConfigurationsManager.kt
+++ core/src/main/kotlin/com/lucasalfare/fltimer/core/configuration/ConfigurationsManager.kt
@@ -1,20 +1,16 @@
 package com.lucasalfare.fltimer.core.configuration
 
-import com.lucasalfare.fllistener.EventManageable
+import com.lucasalfare.fllistening.EventManageable
 import com.lucasalfare.fltimer.core.FLTimerEvent
 import com.lucasalfare.fltimer.core.model.FLTimerState
 
 class ConfigurationsManager : EventManageable() {
 
-  override fun onInitiated() {
-    println("[ConfigurationsManager] Instance inititated.")
+  override suspend fun initialize() {
+    initialized = true
   }
 
-  override fun onNotInitiated() {
-    this.initiated = true
-  }
-
-  override fun onEvent(event: Any, data: Any?, origin: Any?) {
+  override fun onEvent(event: Any, data: Any?) {
     if (event == FLTimerEvent.ConfigSet) {
       val props = data as Array<*>
       val targetConfig = props[0] as Config
