bc91f9d80158c950437159a40122b111aa781a80
LucasAlfare <god-lucas@hotmail.com>
2023-12-16 02:36:15 -0300

    update FLListening dependency version

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -9,16 +9,13 @@ plugins {
 }
 
 dependencies {
-  val flListeningVersion =
-    findProperty("lucasalfare.fllistening.version") ?: ""
-
   val flBinary =
     findProperty("lucasalfare.flbinary.version") ?: ""
 
   implementation(project(":core"))
   implementation(compose.desktop.currentOs)
 
-  implementation("com.github.LucasAlfare:FLListening:$flListeningVersion")
+  implementation("com.github.LucasAlfare:FLListening:v2.0")
   implementation("com.github.LucasAlfare:FLBinary:$flBinary")
 }
 
