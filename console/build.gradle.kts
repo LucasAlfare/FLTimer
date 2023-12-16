9d19af3b876e42ee230fff347ce4e5a42dce737a
LucasAlfare <god-lucas@hotmail.com>
2023-12-16 02:31:37 -0300

    update FLListening dependency version

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -4,14 +4,11 @@ plugins {
 }
 
 dependencies {
-  val flListeningVersion =
-    findProperty("lucasalfare.fllistening.version") ?: ""
   val flBinaryVersion =
     findProperty("lucasalfare.flbinary.version") ?: ""
 
   implementation(compose.runtime)
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
   implementation("com.github.ajalt.mordant:mordant:2.0.0-beta13")
-  implementation("com.github.LucasAlfare:FLListening:$flListeningVersion")
+  implementation("com.github.LucasAlfare:FLListening:v2.0")
   implementation("com.github.LucasAlfare:FLBinary:$flBinaryVersion")
 }
