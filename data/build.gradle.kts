faf534e5a476bde372f9733ca89e229f46a9c598
LucasAlfare <god-lucas@hotmail.com>
2023-12-17 01:35:52 -0300

    include test dependency for data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
   implementation("org.jetbrains.exposed:exposed-core:0.45.0")
   implementation("org.jetbrains.exposed:exposed-jdbc:0.45.0")
   implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+  testImplementation(kotlin("test"))
 }
