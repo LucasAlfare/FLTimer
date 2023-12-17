634079982f855c16f177830ab92b4b64abc3a2f5
LucasAlfare <god-lucas@hotmail.com>
2023-12-17 19:40:55 -0300

    fix in memory database for tests

--- data/src/test/kotlin/DatabaseTests.kt
+++ data/src/test/kotlin/DatabaseTests.kt
@@ -1,4 +1,3 @@
-import com.lucasalfare.fltimer.data.DATABASE_URL
 import com.lucasalfare.fltimer.data.SessionsTable
 import com.lucasalfare.fltimer.data.SolvesTable
 import com.lucasalfare.fltimer.data.initDatabase
@@ -10,13 +9,12 @@ import kotlin.test.assertTrue
 class DatabaseTests {
 
   // in memory target URL
-  private val testDatabaseUrl = "jdbc:sqlite:file:test?mode=memory&cache=shared"
+  private val testDatabaseUrl = "jdbc:sqlite:file:mode=memory"
 
   @Test
   fun `initDatabase should create tables`() {
     // Call the function you want to test
-    // note: with actual file works, but with in memory, don't
-    initDatabase(DATABASE_URL)
+    initDatabase(testDatabaseUrl)
 
     // Verify that tables are created
     transaction {
