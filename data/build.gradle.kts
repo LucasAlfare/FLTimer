ca71d9436b112c6ec8c65b98a3bd4d9f84f8a7bc
LucasAlfare <god-lucas@hotmail.com>
2023-12-17 19:08:37 -0300

    include mockk test dependency

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -9,5 +9,6 @@ dependencies {
   implementation("org.jetbrains.exposed:exposed-core:0.45.0")
   implementation("org.jetbrains.exposed:exposed-jdbc:0.45.0")
   implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+  testImplementation("io.mockk:mockk:1.13.8")
   testImplementation(kotlin("test"))
 }
