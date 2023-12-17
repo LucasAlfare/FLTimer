a2d348ff8f5104da15ad59c5a4c8754b83076bcb
LucasAlfare <god-lucas@hotmail.com>
2023-12-17 19:06:37 -0300

    backing to using exposed to handle database

--- data/src/main/kotlin/Model.kt
+++ data/src/main/kotlin/Model.kt
@@ -1,7 +1,21 @@
+/**
+ * Enum class representing different penalty types that can be applied to a solve.
+ * - [Ok]: No penalty
+ * - [PlusTwo]: Plus two seconds penalty
+ * - [Dnf]: Did not finish penalty
+ */
 enum class Penalty {
-  ok, plus_two, dnf
+  Ok, PlusTwo, Dnf
 }
 
+/**
+ * Data class representing a solve in a cubing session.
+ * @property id Unique identifier for the solve.
+ * @property time Time taken to complete the solve in milliseconds.
+ * @property scramble String representation of the scramble algorithm for the solve.
+ * @property penalty The penalty applied to the solve, as defined by the [Penalty] enum.
+ * @property comment Additional comments or notes about the solve.
+ */
 data class Solve(
   val id: Int,
   val time: Long,
@@ -10,6 +24,11 @@ data class Solve(
   val comment: String
 )
 
+/**
+ * Data class representing a cubing session.
+ * @property id Unique identifier for the session.
+ * @property name Name or description of the session.
+ */
 data class Session(
   val id: Int,
   val name: String
