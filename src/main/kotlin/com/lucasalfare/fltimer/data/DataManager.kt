087968ffefe05288fc2595265bfddcb911d1cdac
LucasAlfare <god-lucas@hotmail.com>
2024-05-31 11:12:46 -0300

    clean up

--- src/main/kotlin/com/lucasalfare/fltimer/data/DataManager.kt
+++ src/main/kotlin/com/lucasalfare/fltimer/data/DataManager.kt
@@ -1,17 +1,15 @@
 package com.lucasalfare.fltimer.data
 
 import com.lucasalfare.fllistening.EventManageable
+import com.lucasalfare.fltimer.Constants
 import com.lucasalfare.fltimer.TimerEvent
+import kotlinx.coroutines.delay
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class DataManager : EventManageable() {
   override suspend fun initialize() {
-    newSuspendedTransaction(db = Datasource.DB) {
-      SchemaUtils.createMissingTablesAndColumns(
-        SessionsTable, SolvesTable
-      )
-    }
+    initDatabase()
 
     initialized = true
   }
@@ -29,6 +27,29 @@ class DataManager : EventManageable() {
     if (event == TimerEvent.TimerFinish) {
       // here we assume that scramble and penalty was previously received; if not, define then as defaults.
       val solveTime = data as Long
+    }
+  }
+
+  private fun initDatabase(
+    dropTablesOnStart: Boolean = false
+  ) {
+    Datasource.initialize(
+      jdbcUrl = System.getenv("DB_JDBC_URL") ?: Constants.SQLITE_URL,
+      jdbcDriverClassName = System.getenv("DB_JDBC_DRIVER") ?: Constants.SQLITE_DRIVER,
+      username = System.getenv("DB_USERNAME") ?: "",
+      password = System.getenv("DB_PASSWORD") ?: ""
+    ) {
+      if (dropTablesOnStart) {
+        SchemaUtils.drop(
+          SessionsTable, SolvesTable
+        )
+      }
+
+      transaction(Datasource.DB) {
+        SchemaUtils.createMissingTablesAndColumns(
+          SessionsTable, SolvesTable
+        )
+      }
     }
   }
 }
