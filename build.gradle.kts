7d97aaa3e8b18cfc37a79910e39aa413d97623c0
LucasAlfare <god-lucas@hotmail.com>
2025-05-26 00:54:18 -0300

    udpate

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,10 @@
-@file:Suppress("PropertyName")
-
 group = "com.lucasalfare.fltimer"
 version = "1.0"
 
-val exposed_version: String by project
-val kotlinVersion: String by project
-
 plugins {
-  kotlin("jvm") version "2.0.0"
-  kotlin("plugin.compose") version "2.0.0"
-  id("org.jetbrains.compose") version "1.6.11"
-//  application
+  alias(libs.plugins.kotlin.jvm)
+  kotlin("plugin.compose") version "2.0.0" // TODO: update
+  id("org.jetbrains.compose") version "1.6.11" // TODO: update
 }
 
 repositories {
@@ -22,23 +16,13 @@ repositories {
 }
 
 dependencies {
+  implementation(libs.exposed.core)
+  implementation(libs.exposed.jdbc)
+  implementation(libs.h2)
+  implementation(libs.sqlite)
+  implementation(libs.logback)
   implementation(compose.desktop.currentOs)
-
-  // dependency to listen key and/or mouse without GUI
-  // used when implementing fltimer in a console/terminal.
   implementation("com.1stleg:jnativehook:2.0.2")
-
-  /*
-  -> Dependencies for database manipulation
-  Database.connect("jdbc:sqlite:/data/data.db", "org.sqlite.JDBC")
-  TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
-   */
-  implementation("org.xerial:sqlite-jdbc:3.46.0.1")
-  implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-  implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-
-//  implementation("ch.qos.logback:logback-classic")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
@@ -48,22 +32,9 @@ compose.desktop {
   }
 }
 
-//application {
-//  mainClass.set("com.lucasalfare.fltimer.MainKt")
-//}
-
 kotlin {
-  jvmToolchain(17)
+  jvmToolchain(21)
 }
-
-//tasks.withType<Jar> {
-//  manifest {
-//    attributes["Main-Class"] = application.mainClass
-//  }
-//
-//  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-//  from(configurations.compileClasspath.map { config -> config.map { if (it.isDirectory) it else zipTree(it) } })
-//}
 
 /**
  * Task used to re-generate wrappers, if needed.
--- src/main/kotlin/com/lucasalfare/fltimer/a_domain/models/Session.kt
+++ src/main/kotlin/com/lucasalfare/fltimer/a_domain/models/Session.kt
@@ -1,7 +1,7 @@
 package com.lucasalfare.fltimer.a_domain.models
 
 data class Session(
-  val id: Int,
+  val id: Int = -1,
   val name: String,
   val solves: List<Solve>
 )
